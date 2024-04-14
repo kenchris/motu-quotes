4a9f0e884ba330c89eb7f05c0152c4b12fa3a2f1
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2024-04-14 15:19:19 +0200

    Add missing files

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'docs/',
+    emptyOutDir: true
+  },
+  plugins: []
+})
