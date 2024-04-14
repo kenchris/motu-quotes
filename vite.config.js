72d292c79338f45e35dddb568ebede8747ce8449
Kenneth Rohde Christiansen <kenneth.r.christiansen@intel.com>
2024-04-14 15:27:59 +0200

    Trying to fix deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/motu-quotes/',
   build: {
     outDir: 'docs/',
     emptyOutDir: true
